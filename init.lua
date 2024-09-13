86dcf5438b515d1a1dd8811d87285b383f26e3f8
Nikita K <obama.library@nara.gov>
2024-09-13 11:56:10 +0300

    Init configuration with main plugins

---
+++ init.lua
@@ -0,0 +1,23 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+require("config.lazy")
+
+vim.cmd('colorscheme catppuccin')
+vim.opt.clipboard = 'unnamedplus'
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+  ensure_installed = {"lua", "javascript", "c", "ruby", "go", "html"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
