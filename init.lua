a5e508f97e013d0f738db50ab3504cfc4bc279cd
Nikita K <obama.library@nara.gov>
2024-09-13 13:03:52 +0300

    Refactor

--- init.lua
+++ init.lua
@@ -6,12 +6,13 @@ vim.g.mapleader = " "
 
 require("config.lazy")
 
-vim.cmd('colorscheme catppuccin')
 vim.opt.clipboard = 'unnamedplus'
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 local configs = require("nvim-treesitter.configs")
 
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,10 @@
+return {
+  { 'projekt0n/github-nvim-theme', name = "github_dark", priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'github_dark_tritanopia'
+    end
+  },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+}
+
