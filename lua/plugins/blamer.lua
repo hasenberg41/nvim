33becb3b3088a665725c0524ca4a293186b6d1d2
Nikita K <obama.library@nara.gov>
2024-10-14 14:54:47 +0300

    blamer patch (gitlens)

---
+++ lua/plugins/blamer.lua
@@ -0,0 +1,9 @@
+return {
+  "APZelos/blamer.nvim",
+  config = function()
+    vim.g.blamer_enabled = 1
+    vim.g.blamer_show_in_visual_modes = 0
+    vim.g.blamer_show_in_insert_modes = 0
+    vim.g.blamer_relative_time = 1
+  end,
+}
