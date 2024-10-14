745390deccdeac3588e320741ca453d3fb9cda51
Nikita K <obama.library@nara.gov>
2024-10-14 14:52:24 +0300

    create own test keybindings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,7 +5,34 @@ return {
     config = function()
       require("toggleterm").setup({})
 
-      vim.keymap.set("n", "<leader><cr>", ":ToggleTerm direction=float<CR>", {})
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+        -- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+        -- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        -- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        -- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        -- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+      end
+
+      function _G._run_rspec_current_file()
+        local file = vim.fn.expand("%")
+        local term_cmd = "rspec " .. file
+        require("toggleterm").exec(term_cmd, 1, 12, vim.o.shell, "horizontal")
+      end
+
+      function _G._run_rspec_all()
+        local term_cmd = "rspec"
+        require("toggleterm").exec(term_cmd, 1, 12, vim.o.shell, "horizontal")
+      end
+
+      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+      vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
+
+      vim.keymap.set("n", "<C-\\>", ":ToggleTerm direction=float<CR>", {})
+      vim.keymap.set("n", "<leader>t", ":lua _run_rspec_current_file()<CR>", {})
+      vim.keymap.set("n", "<leader>T", ":lua _run_rspec_all()<CR>", {})
     end,
   },
 }
