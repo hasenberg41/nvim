62c0986ec9f4a62ba303fd04b80ed0bcb1f8f7be
Nikita K <obama.library@nara.gov>
2024-10-30 15:59:39 +0300

    Refactor ruby test running

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,41 +1,53 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("toggleterm").setup({})
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({})
 
-      function _G.set_terminal_keymaps()
-        local opts = { buffer = 0 }
-        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-      end
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+			end
 
-      function _G._run_rspec_current_file()
-        local file = vim.fn.expand("%")
-        local term_cmd = "rspec " .. file
-        require("toggleterm").exec(term_cmd, 1, 12, vim.o.shell, "horizontal")
-      end
+			local function run_command_in_terminal(cmd)
+				require("toggleterm").exec(cmd, 1, 12, vim.o.shell, "horizontal")
+			end
 
-      function _G._run_rspec_all()
-        local term_cmd = "rspec"
-        require("toggleterm").exec(term_cmd, 1, 12, vim.o.shell, "horizontal")
-      end
+			local function is_ruby_file()
+				return vim.fn.expand("%:e") == "rb"
+			end
 
-      function _G._copy_rspec_command()
-        local file = vim.fn.expand("%")
-        local command = "rspec " .. file
-        vim.fn.setreg("+", command)
-        print("Скопировано в буфер обмена: " .. command)
-      end
+			local function run_rspec_current_file()
+				if is_ruby_file() then
+					local file = vim.fn.expand("%")
+					run_command_in_terminal("rspec " .. file)
+        else
+          print("Not ruby file")
+				end
+			end
 
-      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-      vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
+			local function run_rspec_all()
+				run_command_in_terminal("rspec")
+			end
 
-      vim.keymap.set("n", "<C-\\>", ":ToggleTerm direction=float<CR>", {})
-      vim.keymap.set("n", "<leader>tt", ":lua _run_rspec_current_file()<CR>", {})
-      vim.keymap.set("n", "<leader>tT", ":lua _run_rspec_all()<CR>", {})
-      vim.keymap.set("n", "<leader>ty", ":lua _copy_rspec_command()<CR>", {})
-    end,
-  },
+			local function copy_rspec_command()
+				if is_ruby_file() then
+          local command = "rspec " .. vim.fn.expand("%")
+          vim.fn.setreg("+", command)
+          print("Copied to clipboard: " .. command)
+        else
+          print("Not ruby file")
+        end
+			end
+
+			vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
+
+			vim.keymap.set("n", "<C-\\>", ":ToggleTerm direction=float<CR>", {})
+			vim.keymap.set("n", "<leader>tt", run_rspec_current_file, {})
+			vim.keymap.set("n", "<leader>tT", run_rspec_all, {})
+			vim.keymap.set("n", "<leader>ty", copy_rspec_command, {})
+		end,
+	},
 }
