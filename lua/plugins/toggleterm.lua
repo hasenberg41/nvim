cc9ba5231ac5bfdad963b21894e527f0add6ce79
Nikita K <obama.library@nara.gov>
2024-10-30 15:47:38 +0300

    add copy test command to clipboard

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -8,11 +8,6 @@ return {
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
         vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-        -- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-        -- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-        -- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-        -- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-        -- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
         vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
       end
 
@@ -27,12 +22,20 @@ return {
         require("toggleterm").exec(term_cmd, 1, 12, vim.o.shell, "horizontal")
       end
 
+      function _G._copy_rspec_command()
+        local file = vim.fn.expand("%")
+        local command = "rspec " .. file
+        vim.fn.setreg("+", command)
+        print("Скопировано в буфер обмена: " .. command)
+      end
+
       -- if you only want these mappings for toggle term use term://*toggleterm#* instead
       vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
 
       vim.keymap.set("n", "<C-\\>", ":ToggleTerm direction=float<CR>", {})
-      vim.keymap.set("n", "<leader>t", ":lua _run_rspec_current_file()<CR>", {})
-      vim.keymap.set("n", "<leader>T", ":lua _run_rspec_all()<CR>", {})
+      vim.keymap.set("n", "<leader>tt", ":lua _run_rspec_current_file()<CR>", {})
+      vim.keymap.set("n", "<leader>tT", ":lua _run_rspec_all()<CR>", {})
+      vim.keymap.set("n", "<leader>ty", ":lua _copy_rspec_command()<CR>", {})
     end,
   },
 }
