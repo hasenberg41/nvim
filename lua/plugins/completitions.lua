54a3258c98f559acbb46583c28822efd75d4ffc0
Nikita K <obama.library@nara.gov>
2024-11-01 16:14:59 +0300

    Update and set default codeium keymaps

--- lua/plugins/completitions.lua
+++ lua/plugins/completitions.lua
@@ -5,12 +5,6 @@ return {
 	{
 		"Exafunction/codeium.vim",
 		event = "BufEnter",
-    config = function ()
-      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-      vim.keymap.set('i', '<C-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-      vim.keymap.set('i', '<C-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-    end
 	},
 	{
 		"L3MON4D3/LuaSnip",
