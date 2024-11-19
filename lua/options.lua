7e67ff46e985e6523dfbd0fb0e14fecdf3591eeb
Nikita Kondrashov <>
2024-11-19 10:33:08 +0300

    Set relativenumber

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
 vim.api.nvim_create_autocmd({"VimLeave", "VimSuspend"}, {
