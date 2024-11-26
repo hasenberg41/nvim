7d1cff9eb221e2f333c4fa798f18d29be69a424f
Nikita Kondrashov <>
2024-11-26 16:50:49 +0300

    smart numbertoggle

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,14 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set relativenumber")
+vim.cmd([[
+  :set nu
+  :augroup numbertoggle
+    :autocmd!
+    :autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+    :autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+  :augroup END
+]])
 vim.g.mapleader = " "
 
 vim.api.nvim_create_autocmd({"VimLeave", "VimSuspend"}, {
